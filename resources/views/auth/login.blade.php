f999bcbe681fe802a3de44668aa014a0c97950b7
Pommering <eduardopommering@hotmail.com>
2022-02-10 00:49:39 -0300

    Finalizando Login e Elementos de estilo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,15 +48,15 @@
                 @csrf
 
                 <div class="input-group mb-3">
-                    <input type="email"
-                           name="email"
-                           value="{{ old('email') }}"
-                           placeholder="Email"
-                           class="form-control @error('email') is-invalid @enderror">
+                    <input type="text"
+                           name="username"
+                           value="{{ old('username') }}"
+                           placeholder="Username"
+                           class="form-control @error('username') is-invalid @enderror">
                     <div class="input-group-append">
-                        <div class="input-group-text"><span class="fas fa-envelope"></span></div>
+                        <div class="input-group-text"><span class="fas fa-user"></span></div>
                     </div>
-                    @error('email')
+                    @error('username')
                     <span class="error invalid-feedback">{{ $message }}</span>
                     @enderror
                 </div>
@@ -64,7 +64,7 @@
                 <div class="input-group mb-3">
                     <input type="password"
                            name="password"
-                           placeholder="Password"
+                           placeholder="Senha"
                            class="form-control @error('password') is-invalid @enderror">
                     <div class="input-group-append">
                         <div class="input-group-text">
@@ -81,22 +81,19 @@
                     <div class="col-8">
                         <div class="icheck-primary">
                             <input type="checkbox" id="remember">
-                            <label for="remember">Remember Me</label>
+                            <label for="remember">Mantenha-me conectado</label>
                         </div>
                     </div>
 
                     <div class="col-4">
-                        <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+                        <button type="submit" class="btn btn-primary btn-block">Entrar</button>
                     </div>
 
                 </div>
             </form>
 
-            <p class="mb-1">
-                <a href="{{ route('password.request') }}">I forgot my password</a>
-            </p>
             <p class="mb-0">
-                <a href="{{ route('register') }}" class="text-center">Register a new membership</a>
+                <a href="{{ route('register') }}" class="text-center">Não possuo cadastro</a>
             </p>
         </div>
         <!-- /.login-card-body -->
