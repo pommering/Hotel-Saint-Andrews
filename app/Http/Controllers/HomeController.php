70ebba627ae0f6534ff1555b732ea6c90b031f91
Pommering <eduardopommering@hotmail.com>
2022-02-19 13:13:50 -0300

    implementando graficos

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Gate;
 
 class HomeController extends Controller
 {
@@ -23,6 +26,89 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+
+        /*SELECT MIN(time_execution) FROM `clean_room_tarefas` INNER JOIN clean_rooms on clean_room_tarefas.clean_room_id = clean_rooms.id
+WHERE YEAR(start_date) = YEAR(NOW()) AND MONTH(start_date) = MONTH(NOW());*/
+
+        if (Gate::allows('manager')) {
+            $time_work = DB::table('clean_room_tarefas')
+                ->selectRaw('* ,SEC_TO_TIME(SUM(TIME_TO_SEC(`time_execution`))) as time_work')
+                ->join('clean_rooms', 'clean_room_tarefas.clean_room_id', '=', 'clean_rooms.id')
+                ->whereYear('start_date', date('Y'))
+                ->whereMonth('start_date', date('m'))
+                ->first();
+
+            if(empty($time_work->time_work)) {
+                $time_work->time_work = 0;
+            }
+
+            $all_clear = DB::table('clean_room_tarefas')
+                ->selectRaw('count(*) as all_clear')
+                ->join('clean_rooms', 'clean_room_tarefas.clean_room_id', '=', 'clean_rooms.id')
+                ->whereYear('start_date', date('Y'))
+                ->whereMonth('start_date', date('m'))
+                ->first();
+
+            if(empty($all_clear->all_clear)) {
+                $all_clear->all_clear = 0;
+            }
+
+            $fast_time = DB::table('clean_room_tarefas')
+                ->selectRaw('MIN(time_execution) as fast_time')
+                ->join('clean_rooms', 'clean_room_tarefas.clean_room_id', '=', 'clean_rooms.id')
+                ->whereYear('start_date', date('Y'))
+                ->whereMonth('start_date', date('m'))
+                ->first();
+
+            if(empty($fast_time->fast_time)) {
+                $fast_time->fast_time = 0;
+            }
+
+        } else {
+
+            $time_work = DB::table('clean_room_tarefas')
+                ->selectRaw('* ,SEC_TO_TIME(SUM(TIME_TO_SEC(`time_execution`))) as time_work')
+                ->join('clean_rooms', 'clean_room_tarefas.clean_room_id', '=', 'clean_rooms.id')
+                ->join('users', 'users.id', '=', 'clean_rooms.user_id')
+                ->whereYear('start_date', date('Y'))
+                ->whereMonth('start_date', date('m'))
+                ->where('user_id', Auth::user()->id)
+                ->first();
+
+            if(empty($time_work->time_work)) {
+                $time_work->time_work = 0;
+            }
+
+            $all_clear = DB::table('clean_room_tarefas')
+                ->selectRaw('count(*) as all_clear')
+                ->join('clean_rooms', 'clean_room_tarefas.clean_room_id', '=', 'clean_rooms.id')
+                ->join('users', 'users.id', '=', 'clean_rooms.user_id')
+                ->whereYear('start_date', date('Y'))
+                ->whereMonth('start_date', date('m'))
+                ->where('user_id', Auth::user()->id)
+                ->first();
+
+            if(empty($all_clear->all_clear)) {
+                $all_clear->all_clear = 0;
+            }
+
+            $fast_time = DB::table('clean_room_tarefas')
+                ->selectRaw('MIN(time_execution) as fast_time')
+                ->join('clean_rooms', 'clean_room_tarefas.clean_room_id', '=', 'clean_rooms.id')
+                ->join('users', 'users.id', '=', 'clean_rooms.user_id')
+                ->whereYear('start_date', date('Y'))
+                ->whereMonth('start_date', date('m'))
+                ->where('user_id', Auth::user()->id)
+                ->first();
+
+            if(empty($fast_time->fast_time)) {
+                $fast_time->fast_time = 0;
+            }
+        }
+
+        return view('home')
+            ->with('dataInformation',$time_work->time_work)
+            ->with('clears', $all_clear->all_clear)
+            ->with('fast_time', $fast_time->fast_time);
     }
 }
